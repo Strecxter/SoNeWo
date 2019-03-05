c1d47fb2c0a128ef34c0e5ab3d17d6213428ca61
Strecxter <nightcrafttv@gmail.com>
2019-03-05 19:11:07 +0300

    Add page About

--- main/forms.py
+++ main/forms.py
@@ -1,9 +1,13 @@
 from django import forms
-
+from django.forms import ModelForm, Textarea
 from .models import Post
 
 class PostForm(forms.ModelForm):
-
-    class Meta:
-        model = Post
-        fields = ('title', 'text',)
+	class Meta:
+		model = Post
+		fields = ('title', 'text', 'detailed_text')
+		widgets = {
+            'title': Textarea(attrs={'cols': 80, 'rows': 1}),
+			'text': Textarea(attrs={'cols': 80, 'rows': 4}),
+			'detailed_text': Textarea(attrs={'cols': 80, 'rows': 8}),
+        }
--- main/views.py
+++ main/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 
+def about(request):
+    return render(request, 'main/about.html')
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'main/post_list.html', {'posts': posts})
